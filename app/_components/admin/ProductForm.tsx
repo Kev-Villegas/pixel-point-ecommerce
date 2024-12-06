15272854f868ca40c6809b83a1fb330243a6ee41
Nahu258 <nahugc258@gmail.com>
2024-12-06 13:06:53 -0300

    feat: add cloudinary config for image storage

--- app/_components/admin/ProductForm.tsx
+++ app/_components/admin/ProductForm.tsx
@@ -1,7 +1,12 @@
 "use client";
+import { CldUploadButton } from "next-cloudinary";
 import { useState } from "react";
 import { ReactSortable } from "react-sortablejs";
 import Spinner from "./Spinner";
+
+interface CloudinaryResult {
+  secure_url: string;
+}
 
 type Category = {
   _id: string; // El ID único de la categoría
@@ -12,7 +17,7 @@ export default function ProductForm() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
-  const [images, setImages] = useState([]);
+  const [images, setImages] = useState<string[]>([]);
   const [category, setCategory] = useState("");
   const [productProperties, setProductProperties] = useState({});
 
@@ -24,23 +29,6 @@ export default function ProductForm() {
 
   const updateImagesOrder = (images) => {
     setImages(images);
-  };
-
-  const uploadImages = async (e) => {
-    const files = e.target?.files;
-    if (files?.length > 0) {
-      setIsUploading(true);
-      const data = new FormData();
-
-      for (const file of files) {
-        data.append("file", file);
-      }
-      const res = await axios.post("/api/upload", data);
-      setImages((oldImages) => {
-        return [...oldImages, ...res.data.links];
-      });
-      setIsUploading(false);
-    }
   };
 
   return (
@@ -85,7 +73,35 @@ export default function ProductForm() {
             <Spinner />
           </div>
         )}
-        <label className="flex h-24 w-24 cursor-pointer flex-col items-center justify-center gap-1 rounded-sm border border-primary bg-white text-center text-sm text-primary shadow-sm">
+        <CldUploadButton
+          className="flex h-24 w-24 cursor-pointer flex-col items-center justify-center gap-1 rounded-sm border border-primary bg-white text-center text-sm text-primary shadow-sm"
+          uploadPreset="pixelimages"
+          onQueuesEnd={(response: any, widget: any) => widget.close()}
+          onSuccess={(response: any) => {
+            const info = response.info as CloudinaryResult;
+            setImages((oldImages) => {
+              return [...oldImages, info.secure_url];
+            });
+            console.log(images);
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          <div>Agregar</div>
+        </CldUploadButton>
+        {/* <label className="flex h-24 w-24 cursor-pointer flex-col items-center justify-center gap-1 rounded-sm border border-primary bg-white text-center text-sm text-primary shadow-sm">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -102,7 +118,7 @@ export default function ProductForm() {
           </svg>
           <div>Agregar</div>
           <input type="file" className="hidden" onChange={uploadImages} />
-        </label>
+        </label> */}
       </div>
 
       <label>Descripción</label>
