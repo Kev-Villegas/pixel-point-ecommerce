0376e5518e32287e2d123b855aeea6058990679a
Nahu258 <nahugc258@gmail.com>
2025-01-01 22:22:25 -0300

    fix: mercadopago response

--- app/api/checkout/route.tsx
+++ app/api/checkout/route.tsx
@@ -12,11 +12,10 @@ export async function POST(request: NextRequest) {
     accessToken: process.env.ACCESS_TOKEN as string,
   });
   const payment = new Payment(client);
-  payment
-    .create({ body: body })
-    .then((res) => console.log(res))
-    .catch((e) => console.log(e));
+  payment.create({ body: body });
+  // .then((res) => console.log(res))
+  // .catch((e) => console.log(e));
 
-  return NextResponse.json("ok", { status: 201 });
+  return NextResponse.json({ payment });
   // return NextResponse.json(cartProducts, { status: 201 });
 }
