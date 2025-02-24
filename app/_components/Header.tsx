6711f4138337d315bcf31b7765dce25988dd3568
Kev-Villegas <kevin.villegas1973@gmail.com>
2025-02-24 12:32:16 -0300

    feat: build UserDropDownMenu component

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { authOptions } from "../_lib/authOptions";
 import CartInfo from "./navbar/CartInfo";
 import SearchInput from "./navbar/SearchInput";
+import UserDropDownMenu from "./UserDropDownMenu";
 
 export default async function Header() {
   const brands = [
@@ -121,23 +122,11 @@ export default async function Header() {
               )}
             </Link> */}
             {session && session.user?.email ? (
-              <Link href="/auth/signout">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25"
-                  />
-                </svg>
-              </Link>
+              <UserDropDownMenu session={session} />
             ) : (
+              // <Link href="/auth/signout">
+
+              // </Link>
               <Link href="/auth/signin">
                 <User className="h-6 w-6" />
                 <span className="sr-only">Login</span>
