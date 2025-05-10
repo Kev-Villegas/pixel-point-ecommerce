466b32ce02c15e2415feec5ecb0c798a3d76e880
Nahu258 <nahugc258@gmail.com>
2025-05-10 20:01:52 -0300

    chore: add next-sitemap configuration and generate sitemap files

---
+++ next-sitemap.config.ts
@@ -0,0 +1,9 @@
+import type { IConfig } from "next-sitemap";
+
+const config: IConfig = {
+  siteUrl: "https://www.pixel-point.com.ar",
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+};
+
+export default config;
