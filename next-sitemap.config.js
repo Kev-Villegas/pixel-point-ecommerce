9b466233213d70283c12cde80337729b87b14546
Nahu258 <nahugc258@gmail.com>
2025-05-10 20:15:52 -0300

    fix: change sitemap config file from .ts to .js

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.pixel-point.com.ar',
+  generateRobotsTxt: true, 
+  sitemapSize: 5000
+}
--- next-sitemap.config.ts
+++
@@ -1,9 +0,0 @@
-import type { IConfig } from "next-sitemap";
-
-const config: IConfig = {
-  siteUrl: "https://www.pixel-point.com.ar",
-  generateRobotsTxt: true,
-  sitemapSize: 5000,
-};
-
-export default config;
