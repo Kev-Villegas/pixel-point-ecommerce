cf4a59e90e13318eb52439b1819a513d5ad6ab3b
Nahu258 <nahugc258@gmail.com>
2025-07-07 20:52:40 -0300

    feat: add dynamic Open Graph metadata to product pages

--- app/productos/[id]/page.tsx
+++ app/productos/[id]/page.tsx
@@ -1,10 +1,56 @@
 import ProductDetailsPage from "../_components/ProductDetailsPage";
 
+import { Metadata } from "next";
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const id = params.id;
+
+  const producto = await fetch(
+    `https://pixel-point.com.ar/api/productos/${id}`,
+    {
+      cache: "no-store",
+    },
+  ).then((res) => res.json());
+
+  const primeraImagen = producto.images?.[0]?.url;
+
+  const imagenUrl = primeraImagen?.startsWith("http")
+    ? primeraImagen
+    : `https://pixel-point.com.ar${primeraImagen}`;
+
+  return {
+    title: `${producto.name} - Pixel Point`,
+    description: producto.description,
+    openGraph: {
+      title: `${producto.name} - Pixel Point`,
+      description: producto.description,
+      url: `https://pixel-point.com.ar/productos/${id}`,
+      type: "website",
+      images: primeraImagen
+        ? [
+            {
+              url: imagenUrl,
+              width: 800,
+              height: 600,
+              alt: producto.name,
+            },
+          ]
+        : [],
+    },
+  };
+}
+
 const ProductPage = () => {
   return (
     <div>
       {/* <Header /> */}
-      <ProductDetailsPage />;
+      <ProductDetailsPage />
     </div>
   );
 };
