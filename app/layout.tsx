b49a69b5ddacc7d2ce465c215804c0b9cb349508
Nahu258 <nahugc258@gmail.com>
2025-07-07 14:42:27 -0300

    feat: add and initialize Meta Pixel in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "react-hot-toast";
 import Header from "./_components/Header";
 import SessionWrapper from "./_components/SessionWrapper";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -87,6 +88,8 @@ export const metadata: Metadata = {
   },
 };
 
+const pixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID;
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -95,9 +98,34 @@ export default function RootLayout({
   return (
     <SessionWrapper>
       <html lang="es">
+        <head>
+          {/* Meta Pixel Code */}
+          <Script id="facebook-pixel" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${pixelId}');
+              fbq('track', 'PageView');
+            `}
+          </Script>
+        </head>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
+            />
+          </noscript>
           <React.StrictMode>
             <Header />
             <main>{children}</main>
